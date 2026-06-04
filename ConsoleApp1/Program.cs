6bb1aeabd2fedb7a8e8c6ca318c4d53f48e95b67
jimsh <jimsh@DEMO>
2026-06-04 13:44:39 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿//using System;
+
+//namespace ConsoleApp1
+//{
+//    class Program
+//    {
+//        private static int number;
+
+//        static void Main(string[] args)
+//        {
+//            Console.OutputEncoding = System.Text.Encoding.UTF8; // Ensure console can display Georgian characters
+//            Console.OutputEncoding = System.Text.Encoding.UTF8; // Ensure console can display Georgian characters
+//            Console.WriteLine("შეიყვანეთ რიცხვი; ");
+//            string res = Console.ReadLine();
+//            string result = number % 2 == 0 ? "რიცხვი არის ლუწი" : "რიცხვი არის კენტი";
+//            Console.WriteLine(result);
+
+
+
+//        }
+//    }
+//}
+
+
+
+
+//using System;
+
+
+//{
+//    Console.OutputEncoding = System.Text.Encoding.UTF8; // Ensure console can display Georgian characters
+//    Console.InputEncoding = System.Text.Encoding.UTF8; // Ensure console can read Georgian characters
+
+//    Console.WriteLine("შეიყვანეთ სახელი; ");
+//    string saxeli = Console.ReadLine();
+//    int raodenoba = saxeli.Length;
+//    Console.WriteLine("სიმბოლოების რაოდენობა; " + raodenoba);
+//}
+
+
+
+
+
+
+
+
+//using System;
+//using System.ComponentModel;
+//{
+//    Console.OutputEncoding = System.Text.Encoding.UTF8; // Ensure console can display Georgian characters
+//    Console.InputEncoding = System.Text.Encoding.UTF8; // Ensure console can read Georgian characters
+//    Console.WriteLine("შეიყვანეთ სახელი; ");
+//    string saxeli = Console.ReadLine();
+//    int raodenoba = saxeli.Length;
+//    Console.WriteLine("სიმბოლოების რაოდენობა; " + raodenoba);
+//    Console.WriteLine("დიდი ასოებით: " + saxeli.ToUpper());
+//    Console.WriteLine("პატარა ასოებით: " + saxeli.ToLower());
+//    Console.WriteLine("შეიცავს a-ს: " + saxeli,("Contains", "a-true"));
+
+
+
+
+
+
+}
