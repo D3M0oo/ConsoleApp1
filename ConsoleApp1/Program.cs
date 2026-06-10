8d49f051010654a732b2ec1aabe5c05fa1779673
Demo <jimshiashvilidemo@gmail.com>
2026-06-10 13:48:48 +0400

    Comment out all code in Program.cs
    Commented out existing code for future reference.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,65 +1,84 @@
-﻿//using System;
+//////using System;
 
-//namespace ConsoleApp1
-//{
-//    class Program
-//    {
-//        private static int number;
+//////class Program
+//////{
+//////    static void Main()
+//////    {
 
-//        static void Main(string[] args)
+//////        int[,] numbers =
+//////        {
+//////            {1, 2, 3},//,
+//////            {4, 5, 6}//1;
+//////        };
+
+
+
+//////        for (int i = 0; i < numbers.GetLength(0); i++)
+//////        {
+//////            for (int j = 0; j < numbers.GetLength(1); j++)
+//////            {
+
+//////                Console.WriteLine(numbers[i, j]);
+//////            }
+//////        }
+//////    }
+//////}
+
+
+
+////using System;
+
+////class Program
+////{
+////    static void Main()
+////    {
+
+////        int[,] numbers =
+////        {
+////            {10, 20},
+////            {30, 40}
+////        };
+
+
+////        int sum = 0;
+
+
+////        for (int i = 0; i < numbers.GetLength(0); i++) 
+////        {
+////            for (int j = 0; j < numbers.GetLength(1); j++) 
+////            {
+
+////                sum += numbers[i, j];
+
+////            }
+////        }
+////        Console.OutputEncoding= System.Text.Encoding.UTF8;
+////        Console.WriteLine("ჯამი: " + sum);
+////    }
+////}
+
+//using System;
+
+//int[,] numbers =
 //        {
-//            Console.OutputEncoding = System.Text.Encoding.UTF8; // Ensure console can display Georgian characters
-//            Console.OutputEncoding = System.Text.Encoding.UTF8; // Ensure console can display Georgian characters
-//            Console.WriteLine("შეიყვანეთ რიცხვი; ");
-//            string res = Console.ReadLine();
-//            string result = number % 2 == 0 ? "რიცხვი არის ლუწი" : "რიცხვი არის კენტი";
-//            Console.WriteLine(result);
+//            {15, 7, 20},
+//            {40, 12, 8}
+//        };
 
+//int max = numbers[0, 0];
 
-
+//for (int i = 0; i < numbers.GetLength(0); i++) 
+//{
+//    for (int j = 0; j < numbers.GetLength(1); j++) 
+//    {
+        
+//        if (numbers[i, j] > max)
+//        {
+         
+//            max = numbers[i, j];
 //        }
 //    }
 //}
+//Console.OutputEncoding = System.Text.Encoding.UTF8;
+//Console.WriteLine("მაქსიმალური ელემენტია: " + max);
 
-
-
-
-//using System;
-
-
-//{
-//    Console.OutputEncoding = System.Text.Encoding.UTF8; // Ensure console can display Georgian characters
-//    Console.InputEncoding = System.Text.Encoding.UTF8; // Ensure console can read Georgian characters
-
-//    Console.WriteLine("შეიყვანეთ სახელი; ");
-//    string saxeli = Console.ReadLine();
-//    int raodenoba = saxeli.Length;
-//    Console.WriteLine("სიმბოლოების რაოდენობა; " + raodenoba);
-//}
-
-
-
-
-
-
-
-
-//using System;
-//using System.ComponentModel;
-//{
-//    Console.OutputEncoding = System.Text.Encoding.UTF8; // Ensure console can display Georgian characters
-//    Console.InputEncoding = System.Text.Encoding.UTF8; // Ensure console can read Georgian characters
-//    Console.WriteLine("შეიყვანეთ სახელი; ");
-//    string saxeli = Console.ReadLine();
-//    int raodenoba = saxeli.Length;
-//    Console.WriteLine("სიმბოლოების რაოდენობა; " + raodenoba);
-//    Console.WriteLine("დიდი ასოებით: " + saxeli.ToUpper());
-//    Console.WriteLine("პატარა ასოებით: " + saxeli.ToLower());
-//    Console.WriteLine("შეიცავს a-ს: " + saxeli.Contains("a"));
-
-
-
-
-
-
-}
